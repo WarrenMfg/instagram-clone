88eec894b1926f063c64d44ba5619a9405959641
WarrenMfg <warrenmfg@icloud.com>
2021-02-21 11:02:58 -0500

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
 
-const Dashboard = lazy(() => import('./pages/Dashboard'));
 const Login = lazy(() => import('./pages/Login'));
 const Signup = lazy(() => import('./pages/Signup'));
 const Profile = lazy(() => import('./pages/Profile'));
 const NotFound = lazy(() => import('./pages/NotFound'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
 
 function App() {
   return (
-    <div>
-      <p>Hello there!</p>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGNUP} component={Signup} />
+          <Route path={ROUTES.PROFILE} component={Profile} />
+          <Route path={ROUTES.NOTFOUND} component={NotFound} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
